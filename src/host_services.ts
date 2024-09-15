7eb14f07697a2fa94e31607a4403038c3c3ca34f
Kien La <la.kien@gmail.com>
2024-09-14 21:10:22 -0400

    show warning if ssh auth sock not found

--- src/host_services.ts
+++ src/host_services.ts
@@ -1,4 +1,5 @@
 import { existsSync, linkSync, unlinkSync } from 'fs'
+import icons from './icons'
 import path from 'path'
 
 export function linkSshAuthSock() {
@@ -7,6 +8,11 @@ export function linkSshAuthSock() {
 
   const targetPath = path.join(process.env.STAX_HOST_SERVICES, 'ssh-auth.sock')
 
-  if (existsSync(targetPath)) unlinkSync(targetPath)
-  linkSync(process.env.SSH_AUTH_SOCK, targetPath)
+  if (existsSync(targetPath))
+    unlinkSync(targetPath)
+
+  if (existsSync(process.env.SSH_AUTH_SOCK))
+    linkSync(process.env.SSH_AUTH_SOCK, targetPath)
+  else
+    console.warn(`${icons.warning}  Couldn't create ${targetPath} because SSH_AUTH_SOCK (${process.env.SSH_AUTH_SOCK}) not found`)
 }
