e18e8a484d126747037e8c3c99dca8efc5e9a52b
Kien La <la.kien@gmail.com>
2024-10-17 20:40:37 -0400

    add warning when ssh auth sock can't be symlinked

--- src/host_services.ts
+++ src/host_services.ts
@@ -11,8 +11,12 @@ export function linkSshAuthSock() {
   if (existsSync(targetPath))
     unlinkSync(targetPath)
 
-  if (existsSync(process.env.SSH_AUTH_SOCK))
-    linkSync(process.env.SSH_AUTH_SOCK, targetPath)
-  else
+  if (existsSync(process.env.SSH_AUTH_SOCK)) {
+    try {
+      linkSync(process.env.SSH_AUTH_SOCK, targetPath)
+    } catch (e) {
+      console.log(`${icons.warning}  Couldn't symlink ${process.env.SSH_AUTH_SOCK} to ${targetPath}. Is the socket still valid? - `, e.message)
+    }
+  } else
     console.warn(`${icons.warning}  Couldn't create ${targetPath} because SSH_AUTH_SOCK (${process.env.SSH_AUTH_SOCK}) not found`)
 }
