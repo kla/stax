1026fc288dee54aff876fc9ee1236eca748e4aeb
Kien La <la.kien@gmail.com>
2024-11-24 16:11:40 -0500

    validate setting name

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,9 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs'
+import { exit, verifyDirectory, resolve } from './utils'
 import yaml from 'js-yaml'
 import * as path from 'path'
-import { verifyDirectory, resolve } from './utils'
+
+const VALID_NAMES = [ 'services_home' ]
 
 function filename(): string {
   return path.join(process.env.STAX_HOME, 'settings.yaml')
@@ -11,15 +13,23 @@ function load() {
   return (existsSync(filename()) && yaml.load(readFileSync(filename(), 'utf-8'))) || {}
 }
 
+function validateName(name: string) {
+  if (!VALID_NAMES.includes(name))
+    exit(1, { message: `Invalid setting name: ${name}\nValid names are: ${VALID_NAMES.join(', ')}` } )
+}
+
 const settings = {
   all: function() { return load() },
 
   read: function(name: string, defaultValue: any | undefined = undefined) {
+    validateName(name)
     const settings = load()
     return settings[name] || defaultValue
   },
 
-  write: function(name, value): any {
+  write: function(name: string, value): any {
+    validateName(name)
+
     if (name === 'services_home') verifyDirectory(value = resolve(value))
 
     const settings = { ...load() }
