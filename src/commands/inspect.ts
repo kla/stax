0b2f3eca1d9aeb949420f5b8087c12ab6fe3abca
Kien La <la.kien@gmail.com>
2025-06-14 14:34:07 -0400

    Add --json option to inspect command

--- src/commands/inspect.ts
+++ src/commands/inspect.ts
@@ -4,24 +4,37 @@ import { capture } from '~/shell'
 import { pp } from '~/utils'
 import Stax from '~/stax'
 
+function output(data: any, options: { json?: boolean }) {
+  if (options.json)
+    console.log(JSON.stringify(data))
+  else if (typeof data === 'string')
+    pp(data)
+  else
+    pp(data)
+}
+
 export default function registerInspectCommand(program: Command, stax: Stax) {
   program.command('inspect')
     .argument('[name]', 'Name of application')
     .option('-c, --compose', 'Show the compose file')
     .option('-d, --dockerfile', 'Show the Dockerfile build (if any)')
     .option('-l, --labels', 'Show container labels')
+    .option('-j, --json', 'Output in raw JSON format')
     .description('Inspect the container or build files.')
     .action((name, options) => {
       name = stax.deduceAppName(name)
       const app = stax.find(name)
 
-      if (options.compose)
-        pp(readFileSync(stax.find(name).primary.composeFile, 'utf-8'))
-      else if (options.dockerfile)
+      if (options.compose) {
+        const composeContent = readFileSync(stax.find(name).primary.composeFile, 'utf-8')
+        output(composeContent, options)
+      } else if (options.dockerfile) {
         console.log('TODO')
-      else if (options.labels)
-        console.log(app.primary.labels)
-      else
-        pp(JSON.parse(capture(`docker inspect ${app.primary.containerName}`)))
+      } else if (options.labels) {
+        output(app.primary.labels, options)
+      } else {
+        const inspectData = JSON.parse(capture(`docker inspect ${app.primary.containerName}`))
+        output(inspectData, options)
+      }
     })
 }
